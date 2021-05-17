4de4fb683c93fd33ae47e26e8781ae1f2b5ab6bf
pplomith <plomitallopaolo@gmail.com>
2021-05-17 21:01:33 +0200

    Update PatientDAO with a new method to get the selected patient

--- src/main/java/model/PatientDAO.java
+++ src/main/java/model/PatientDAO.java
@@ -37,12 +37,13 @@ public class PatientDAO {
         return null;
     }
 
-    public JSONArray getVisitDatePatient(String id) {
+    public JSONArray getPatientAndVD(String id) {
 
         try (Connection con = ConPool.getConnection()) {
 
-            PreparedStatement ps = con.prepareStatement("SELECT Patient, Date FROM meddata.patient, meddata.visitdata " +
-                    "WHERE patient.PatientId = ? AND patient.PatientId = visitdata.Patient");
+            PreparedStatement ps = con.prepareStatement("SELECT vd.Patient, p.FirstName, p.LastName, p.BloodType, p.DateOfBirth, p.Gender, p.Height, vd.Date, part.Short_Name, vd.Value " +
+                    "FROM meddata.patient as p, meddata.visitdata as vd, meddata.part as part"
+                    + " WHERE p.PatientId = ? AND vd.Part = part.LOINC AND p.PatientId = vd.Patient");
 
             ps.setString(1,id);
 
@@ -51,7 +52,14 @@ public class PatientDAO {
             while (rs.next()) {
                 JSONObject object = new JSONObject();
                 object.put("ID", rs.getString("Patient"));
+                object.put("Name", rs.getString("FirstName") + " " + rs.getString("LastName"));
+                object.put("BloodType", rs.getString("BloodType"));
+                object.put("DateOfBirth", rs.getString("DateOfBirth"));
+                object.put("Gender", rs.getString("Gender"));
+                object.put("Height", rs.getString("Height"));
                 object.put("Date", rs.getString("Date"));
+                object.put("Measurement", rs.getString("Short_Name"));
+                object.put("Value", rs.getString("Value"));
                 arrayObject.add(object);
             }
             return arrayObject;
