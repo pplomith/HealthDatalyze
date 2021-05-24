023c586d85f7532eb647fe967fd274e17358eb53
pplomith <plomitallopaolo@gmail.com>
2021-05-24 21:20:11 +0200

    Add package, package-lock and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+module.exports = {
+    entry: './src/main/webapp/js/index.js',
+    mode: 'development',
+    output: {
+        path: path.resolve('src/main/webapp/js'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        alias: {
+            'jquery': 'gridstack/dist/jq/jquery.js',
+            'jquery-ui': 'gridstack/dist/jq/jquery-ui.js',
+            'jquery.ui': 'gridstack/dist/jq/jquery-ui.js',
+            'jquery.ui.touch-punch': 'gridstack/dist/jq/jquery.ui.touch-punch.js',
+        },
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+};
