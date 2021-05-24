eb90ef993316f7400d6e46634cc1f14b39b44300
pplomith <plomitallopaolo@gmail.com>
2021-05-24 21:12:48 +0200

    Update index.js with new features

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,7 +1,21 @@
 import {select} from 'd3';
+import ReactDOM from "react-dom";
+import React from "react";
+import { MyFirstGrid } from './initDashboard';
+import { searchPatient } from './searchScript';
+
+const contentDiv = document.getElementById("root");
+const gridProps = window.gridProps || {};
+ReactDOM.render(React.createElement(MyFirstGrid, gridProps), contentDiv);
+
+$('#searchPatient').keyup(searchPatient);
 
 const svg = select('svg');
-const width = +svg.attr('width');
+
+var svgElement = document.getElementById("svgId");
+const width = svgElement.viewBox.baseVal.width;
+const heigth = svgElement.viewBox.baseVal.height;
+
 var patientSelected = null;
 
 
@@ -23,9 +37,6 @@ $(document).ready(function () {
         }
     });
 });
-
-// var button = document.querySelector("#buttonForJson");
-// button.addEventListener("click", getJson);
 
 function getJson() {
     $.ajax({
@@ -64,31 +75,38 @@ function fillPatientTable(data) {
             dataType: 'json',
             data: {"id" : this.id},
             success: function (response) {
-                visitDatePatient(response)
+                visitDatePatient(response);
             }
         });
     });
 }
 
 function visitDatePatient(data) {
+    //the first item of data is patient selected
+    $("#patientName").html(data[0].Name);
+    $("#ageSpan").html(getAge(data[0].DateOfBirth));
+    $("#sexSpan").html(data[0].Gender[0].toUpperCase()+data[0].Gender.slice(1));
+    $("#bloodSpan").html(data[0].BloodType);
     $("#measurementTable").empty();
     $("#tableVisitDate").empty();
+    $('#noteVisit').empty();
     $("#tableVisitDate").off("click","button");
+    console.log(data);
     patientSelected = data;
     var table = null;
-    if(data.length > 0) {
-        var dataVisit = new Date(data[0].Date);
+    if(data.length > 1) {
+        var dataVisit = new Date(data[1].Date);
         var formatDate = dataVisit.getFullYear() + "-" + (dataVisit.getMonth() + 1) + "-" + dataVisit.getDate();
         table += "<tr><td>" + data[0].ID + "</td>"
             + "<td>" + formatDate + "</td>"
-            + "<td><button type='button' class='btn btn-primary' value='"+data[0].Date+"'>Show</button></td>"
+            + "<td><button type='button' class='btn btn-primary' value='"+data[1].Date+"'>Show</button></td>"
             + "</tr>"
         var precedentData = dataVisit;
-        for (var i = 1; i < data.length; i++) {
+        for (var i = 2; i < data.length; i++) {
             dataVisit = new Date(data[i].Date);
             if (dataVisit.getTime() != precedentData.getTime()) {
                 var formatDate = dataVisit.getFullYear() + "-" + (dataVisit.getMonth() + 1) + "-" + dataVisit.getDate();
-                table += "<tr><td>" + data[i].ID + "</td>"
+                table += "<tr><td>" + data[0].ID + "</td>"
                     + "<td>" + formatDate + "</td>"
                     + "<td><button type='button' class='btn btn-primary' value='"+data[i].Date+"'>Show</button></td>"
                     + "</tr>"
@@ -101,27 +119,36 @@ function visitDatePatient(data) {
             measurementTableUpdate(this.value);
         });
     }
+    function getAge(dateOfBirth) {
+        var today = Date.now();
+        var birth = new Date(dateOfBirth);
+        return parseInt((today - birth.getTime()) / (1000*60*60*24*365));
+    }
+}
 
-    function measurementTableUpdate(visitDate) {
-        var table = null;
-        let dataSelected = [];
-        if (patientSelected != null) {
+function measurementTableUpdate(visitDate) {
 
-            for (var i = 0; i < patientSelected.length; i++) {
-                if (patientSelected[i].Date == visitDate) {
-                    dataSelected.push(patientSelected[i]);
-                }
+    var table, cmt = null;
+
+    let dataSelected = [];
+    if (patientSelected != null) {
+
+        for (var i = 1; i < patientSelected.length; i++) {
+            if (patientSelected[i].Date == visitDate) {
+                dataSelected.push(patientSelected[i]);
             }
-            for (var i = 0; i < dataSelected.length; i++) {
-                var vInt = parseFloat(dataSelected[i].Value);
-                var valueRounded = Math.round(vInt * 100) / 100;
-                table += "<tr><td>" + dataSelected[i].Measurement + "</td>"
-                        + "<td>" + valueRounded + "</td>"
-                        + "</tr>"
-            }
-
-            $("#measurementTable").html(table);
         }
 
+        for (var i = 0; i < dataSelected.length; i++) {
+            var vInt = parseFloat(dataSelected[i].Value);
+            var valueRounded = Math.round(vInt * 100) / 100;
+            table += "<tr><td>" + dataSelected[i].Measurement + "</td>"
+                + "<td>" + valueRounded + "</td>"
+                + "</tr>"
+
+        }
+
+        $("#measurementTable").html(table);
     }
+
 }
