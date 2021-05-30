cfe3b053fb2cf9190e341591ba6d4331f7e4eb4f
pplomith <plomitallopaolo@gmail.com>
2021-05-30 21:16:40 +0200

    Update index with new features

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,8 +1,8 @@
-import {select} from 'd3';
 import ReactDOM from "react-dom";
 import React from "react";
 import { MyFirstGrid } from './initDashboard';
-import { searchPatient } from './searchScript';
+import { searchPatient} from './searchScript';
+import { createChart, processData } from './createChart';
 
 const contentDiv = document.getElementById("root");
 const gridProps = window.gridProps || {};
@@ -10,23 +10,101 @@ ReactDOM.render(React.createElement(MyFirstGrid, gridProps), contentDiv);
 
 $('#searchPatient').keyup(searchPatient);
 
-const svg = select('svg');
+// Hide submenus
+$('#body-row .collapse').collapse('hide');
 
-var svgElement = document.getElementById("svgId");
-const width = svgElement.viewBox.baseVal.width;
-const heigth = svgElement.viewBox.baseVal.height;
+// Collapse/Expand icon
+$('#collapse-icon').addClass('fa-angle-double-left');
+
+// Collapse click
+$('[data-toggle=sidebar-colapse]').click(function() {
+    sidebarCollapse();
+});
+
+function sidebarCollapse() {
+    $('.menu-collapsed').toggleClass('d-none');
+    $('.sidebar-submenu').toggleClass('d-none');
+    $('.submenu-icon').toggleClass('d-none');
+    $('#sidebar-container').toggleClass('sidebar-expanded sidebar-collapsed');
+
+    // Treating d-flex/d-none on separators with title
+    var SeparatorTitle = $('.sidebar-separator-title');
+    if ( SeparatorTitle.hasClass('d-flex') ) {
+        SeparatorTitle.removeClass('d-flex');
+    } else {
+        SeparatorTitle.addClass('d-flex');
+    }
+
+    // Collapse/Expand icon
+    $('#collapse-icon').toggleClass('fa-angle-double-left fa-angle-double-right');
+}
+
+
+// //create multiselection
+$(document).ready(function () {
+    $('#selectValue').multiselect({
+        enableFiltering: true,
+        enableCaseInsensitiveFiltering: true,
+        filterPlaceholder:'Search Here...',
+        onChange: function () {
+            var selected = this.$select.val();
+            processData(selected);
+
+            var selectedOptions = jQuery('#selectValue option:selected');
+
+            if(selectedOptions.length >= 4) {
+                var nonSelectedOptions = jQuery('#selectValue option').filter(function () {
+                    return !jQuery(this).is(':selected');
+                });
+
+                nonSelectedOptions.each(function () {
+                    var button = jQuery('#selectValue + .btn-group button[title="'+ jQuery(this).val() +'"]')
+                    var input = jQuery('#selectValue + .btn-group input[value="'+ jQuery(this).val() + '"]');
+                    input.prop('disabled', true);
+                    button.prop('disabled', true);
+                    input.parent('li').addClass('disabled');
+                });
+            }
+            else {
+                jQuery('#selectValue option').each(function () {
+                    var button = jQuery('#selectValue + .btn-group button[title="'+ jQuery(this).val() +'"]')
+                    var input = jQuery('#selectValue + .btn-group input[value="' + jQuery(this).val() + '"]');
+                    input.prop('disabled', false);
+                    button.prop('disabled', false);
+                    input.parent('li').addClass('disabled');
+                });
+            }
+        }
+    });
+});
+
 
 var patientSelected = null;
 
 
-const dataPrint =  (data) => {
-    data.forEach(d => {
-        d.Value = +d.Value;
-        d.Date = new Date(d.Date);
-    });
-    console.log(data);
-}
+// const dataPrint = (data) => {
+//     data.forEach(d => {
+//         d.Value = +d.Value;
+//         d.Date = new Date(d.Date);
+//     });
+//     console.log(data);
+// }
+//
+//
+//
+// function getJson() {
+//     $.ajax({
+//         type: 'POST',
+//         url: 'GetData',
+//         dataType: 'json',
+//         success: function (response) {
+//             dataPrint(response);
+//         }
+//
+//     });
+// }
 
+//get all patients ajax function
 $(document).ready(function () {
     $.ajax({
         type: 'POST',
@@ -37,19 +115,7 @@ $(document).ready(function () {
         }
     });
 });
-
-function getJson() {
-    $.ajax({
-        type: 'POST',
-        url: 'GetData',
-        dataType: 'json',
-        success: function (response) {
-            dataPrint(response);
-        }
-
-    });
-}
-
+//fill patients table function
 function fillPatientTable(data) {
     var table = null;
     for (var i=0; i<data.length;i++) {
@@ -80,7 +146,7 @@ function fillPatientTable(data) {
         });
     });
 }
-
+//fill visit date table function
 function visitDatePatient(data) {
     //the first item of data is patient selected
     $("#patientName").html(data[0].Name);
@@ -91,8 +157,7 @@ function visitDatePatient(data) {
     $("#tableVisitDate").empty();
     $('#noteVisit').empty();
     $("#tableVisitDate").off("click","button");
-    console.log(data);
-    patientSelected = data;
+    patientSelected = data; //patient selected
     var table = null;
     if(data.length > 1) {
         var dataVisit = new Date(data[1].Date);
@@ -118,12 +183,35 @@ function visitDatePatient(data) {
         $("#tableVisitDate").on("click","button", function () {
             measurementTableUpdate(this.value);
         });
+        //call to the funciont to create the chart
+        createChart(patientSelected);
+        fillSelect(data);
     }
     function getAge(dateOfBirth) {
         var today = Date.now();
         var birth = new Date(dateOfBirth);
         return parseInt((today - birth.getTime()) / (1000*60*60*24*365));
     }
+}
+//create options for select
+function fillSelect(data) {
+    $('#selectValue').empty();
+    console.log(data);
+    var options = [];
+    for (var i = 1; i < data.length; i++) {
+        if(!options.includes(data[i].Measurement))
+            options.push(data[i].Measurement);
+    }
+    console.log(options);
+    var select = document.getElementById('selectValue');
+    options.forEach(d => {
+        var opt = document.createElement('option');
+        opt.value = d;
+        opt.innerHTML = d;
+        select.appendChild(opt);
+    });
+
+    $('#selectValue').multiselect('rebuild');
 }
 
 function measurementTableUpdate(visitDate) {
@@ -132,9 +220,9 @@ function measurementTableUpdate(visitDate) {
 
     let dataSelected = [];
     if (patientSelected != null) {
-
+        var date = new Date(visitDate);
         for (var i = 1; i < patientSelected.length; i++) {
-            if (patientSelected[i].Date == visitDate) {
+            if (patientSelected[i].Date.getTime() == date.getTime()) {
                 dataSelected.push(patientSelected[i]);
             }
         }
