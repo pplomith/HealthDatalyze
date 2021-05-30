970786d45ae17c32e4e8bd93b1358277df8a2cab
pplomith <plomitallopaolo@gmail.com>
2021-05-30 21:22:40 +0200

    Add LayoutDAO and LayoutData to manage the layouts

---
+++ src/main/java/controller/LayoutData.java
@@ -0,0 +1,66 @@
+package controller;
+
+
+import model.LayoutDAO;
+import org.json.simple.JSONArray;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/LayoutConfig")
+public class LayoutData extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        doPost(request,response);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        LayoutDAO lDao = new LayoutDAO();
+        String lId = request.getParameter("layoutId");
+        String lName = request.getParameter("layoutName");
+        String lProps = request.getParameter("layouts");
+        String boolDelete = request.getParameter("boolDelete");
+        String boolAllLayout = request.getParameter("boolAllLayout");
+        String result = "";
+        if (boolAllLayout == null) {
+            if (lName != null && lProps != null && lId != null) {
+                boolean rs = lDao.saveLayout(lId, lName, lProps);
+                if (rs) result = "success";
+                else result = "failed";
+            } else if (lId != null && boolDelete == null){
+                String rs = lDao.getLayout(lId);
+                if (rs != null) result = rs;
+                else result = "failed";
+            } else if (lId != null && boolDelete != null) {
+                if(Boolean.parseBoolean(boolDelete)) {
+                    boolean rs = lDao.deleteLayout(lId);
+                    if (rs) result = "success";
+                    else result = "failed";
+                }
+            } else {
+                int rs = lDao.getId();
+                if (rs != -1) result = "" + rs;
+                else result = "failed";
+            }
+
+            response.setContentType("text/plain");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(result);
+        } else {
+            JSONArray jsonObject = null;
+            jsonObject = lDao.getAllLayouts();
+            request.setCharacterEncoding("utf8");
+            response.setContentType("text/plain");
+            PrintWriter out = response.getWriter();
+            System.out.println(jsonObject.toString());
+            out.print(jsonObject.toString());
+        }
+
+
+    }
+}
---
+++ src/main/java/model/LayoutDAO.java
@@ -0,0 +1,106 @@
+package model;
+
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LayoutDAO {
+
+    public boolean saveLayout(String id, String lName, String layouts) {
+
+        try (Connection con = ConPool.getConnection()) {
+
+            PreparedStatement ps = con.prepareStatement("DELETE FROM meddata.layoutconfig WHERE layoutid = ?");
+            ps.setString(1, id);
+            int rs = ps.executeUpdate();
+
+             ps = con.prepareStatement("INSERT INTO meddata.layoutconfig(LayoutId, LayoutName, layouts) values (?, ?, ?)");
+
+             ps.setString(1, id);
+             ps.setString(2, lName);
+             ps.setString(3, layouts);
+
+             rs = ps.executeUpdate();
+             return true;
+        } catch (SQLException throwables) {
+            return false;
+        }
+
+    }
+
+    public String getLayout(String lId) {
+        String result = null;
+        try (Connection con = ConPool.getConnection()) {
+
+            PreparedStatement ps = con.prepareStatement("SELECT layouts FROM meddata.layoutconfig WHERE layoutId = ?");
+            ps.setString(1, lId);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                result = rs.getString("layouts");
+            }
+            return result;
+        } catch (SQLException throwables) {
+            return result;
+        }
+    }
+
+    public int getId() {
+        try (Connection con = ConPool.getConnection()) {
+            int id = -1;
+            PreparedStatement ps = con.prepareStatement("SELECT layoutid FROM (SELECT '1' AS layoutid) q1 WHERE NOT EXISTS (SELECT '1' FROM meddata.layoutconfig WHERE layoutid = '1')" +
+                    "UNION ALL SELECT * FROM (SELECT  layoutid + '1' FROM meddata.layoutconfig t WHERE NOT EXISTS (SELECT '1' FROM meddata.layoutconfig ti WHERE ti.layoutid = t.layoutid + '1' ) ORDER BY layoutid LIMIT 1) q2 ORDER BY layoutid LIMIT 1");
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                id = rs.getInt("layoutid");
+            }
+            return id;
+        } catch (SQLException throwables) {
+            return -1;
+        }
+    }
+
+    public boolean deleteLayout(String id) {
+
+        try (Connection con = ConPool.getConnection()) {
+
+            PreparedStatement ps = con.prepareStatement("DELETE FROM meddata.layoutconfig WHERE layoutid = ?");
+            ps.setString(1, id);
+            int rs = ps.executeUpdate();
+
+            return true;
+        } catch (SQLException throwables) {
+            return false;
+        }
+
+    }
+
+    public JSONArray getAllLayouts() {
+
+        try (Connection con = ConPool.getConnection()) {
+
+            PreparedStatement ps = con.prepareStatement("SELECT layoutId, layoutName FROM meddata.layoutconfig");
+
+            JSONArray arrayObject = new JSONArray();
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                JSONObject object = new JSONObject();
+                object.put("layoutId", rs.getString("layoutId"));
+                object.put("layoutName", rs.getString("layoutName"));
+                arrayObject.add(object);
+            }
+            return arrayObject;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
+        return null;
+    }
+
+}
