c69de42e4786c25eb4cefc36e4969348b242ab1a
pplomith <plomitallopaolo@gmail.com>
2021-07-27 17:26:32 +0200

    Update PatientData with new function

--- src/main/java/controller/PatientData.java
+++ src/main/java/controller/PatientData.java
@@ -1,43 +1,63 @@
 package controller;
 
+import model.Doctor;
 import model.PatientDAO;
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/PatientData")
 public class PatientData extends HttpServlet {
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         doPost(request,response);
     }
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        PatientDAO patientDAO = new PatientDAO();
-        JSONObject jsonObject = null;
-        String patientId = request.getParameter("id");
-
-        if (patientId != null) {
-            jsonObject = patientDAO.getPatientData(patientId);
-        }
-        else {
-            jsonObject = patientDAO.getAllPatients();
-        }
-
-
-        if (jsonObject != null) {
-            request.setCharacterEncoding("utf8");
-            response.setContentType("application/json");
-            PrintWriter out = response.getWriter();
-            out.print(jsonObject.toString());
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            PatientDAO patientDAO = new PatientDAO();
+            JSONObject jsonObject = null;
+            String patientId = request.getParameter("id");
+            String reqId = request.getParameter("requestId");
+            if (patientId != null && reqId == null) {
+                jsonObject = patientDAO.getPatientData(patientId);
+            }
+            else if (reqId == null){
+                jsonObject = patientDAO.getAllPatients();
+            } else if (reqId.equals("105")) {
+                String startDate = request.getParameter("startDate");
+                String endDate = request.getParameter("endDate");
+                String name = request.getParameter("name");
+                String type = request.getParameter("type");
+                String description = request.getParameter("description");
+                String groupId = request.getParameter("groupId");
+                if (endDate.equals("")) endDate = null;
+                if (session.getAttribute("MedDoctor") != null) {
+                    Doctor doc = (Doctor) session.getAttribute("MedDoctor");
+                    patientDAO.doSaveHR(patientId, groupId, name, startDate, endDate, description, type, doc.getId());
+                }
+                jsonObject = patientDAO.getPatientData(patientId);
+            }
+            if (jsonObject != null) {
+                request.setCharacterEncoding("utf8");
+                response.setContentType("application/json");
+                PrintWriter out = response.getWriter();
+                out.print(jsonObject.toString());
+            } else {
+                request.setCharacterEncoding("utf8");
+                response.setContentType("text/plain");
+                response.getWriter().write("Server Error");
+            }
         } else {
-            request.setCharacterEncoding("utf8");
-            response.setContentType("text/plain");
-            response.getWriter().write("Server Error");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/view/login.jsp");
+            dispatcher.forward(request,response);
         }
     }
 }
