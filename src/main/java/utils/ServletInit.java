5fe4896c06fea1d584e86524b2992f076eac7951
pplomith <plomitallopaolo@gmail.com>
2021-07-27 17:25:23 +0200

    Update initial servlet

--- src/main/java/utils/ServletInit.java
+++ src/main/java/utils/ServletInit.java
@@ -1,7 +1,5 @@
 package utils;
 
-import model.PatientDAO;
-import org.json.simple.JSONArray;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,9 +7,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "Home", urlPatterns = "/InitServlet", loadOnStartup = 1)
+@WebServlet(name = "Home", urlPatterns = "", loadOnStartup = 1)
 public class ServletInit extends HttpServlet {
 
     public void init() throws ServletException {
@@ -24,7 +23,8 @@ public class ServletInit extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        HttpSession session = request.getSession(false);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/view/login.jsp");
         dispatcher.forward(request,response);
     }
 
