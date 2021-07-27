8b4c2c95358af2e3330861c4d46eb8b6ee7077d4
pplomith <plomitallopaolo@gmail.com>
2021-07-27 17:28:28 +0200

    Update layoutData with new functions

--- src/main/java/controller/LayoutData.java
+++ src/main/java/controller/LayoutData.java
@@ -1,67 +1,81 @@
 package controller;
 
 
+import model.Doctor;
 import model.LayoutDAO;
 import org.json.simple.JSONArray;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet("/LayoutConfig")
 public class LayoutData extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         doPost(request,response);
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        LayoutDAO lDao = new LayoutDAO();
-        String lId = request.getParameter("layoutId");
-        String lName = request.getParameter("layoutName");
-        String lProps = request.getParameter("layouts");
-        String boolDelete = request.getParameter("boolDelete");
-        String boolAllLayout = request.getParameter("boolAllLayout");
-        String result = "";
-        if (boolAllLayout == null) {
-            if (lName != null && lProps != null && lId != null) {
-                boolean rs = lDao.saveLayout(lId, lName, lProps);
-                if (rs) result = "success";
-                else result = "failed";
-            } else if (lId != null && boolDelete == null){
-                String rs = lDao.getLayout(lId);
-                if (rs != null) result = rs;
-                else result = "failed";
-            } else if (lId != null && boolDelete != null) {
-                if(Boolean.parseBoolean(boolDelete)) {
-                    boolean rs = lDao.deleteLayout(lId);
-                    if (rs) result = "success";
-                    else result = "failed";
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            Doctor doc = (Doctor) session.getAttribute("MedDoctor");
+            if (doc != null) {
+                LayoutDAO lDao = new LayoutDAO();
+                String lId = request.getParameter("layoutId");
+                String lName = request.getParameter("layoutName");
+                String lProps = request.getParameter("layouts");
+                String boolDelete = request.getParameter("boolDelete");
+                String boolAllLayout = request.getParameter("boolAllLayout");
+                String result = "";
+                if (boolAllLayout == null) {
+                    if (lName != null && lProps != null && lId != null) {
+                        boolean rs = lDao.saveLayout(lId, lName, lProps, doc.getId());
+                        if (rs) result = "success";
+                        else result = "failed";
+                    } else if (lId != null && boolDelete == null){
+                        String rs = lDao.getLayout(lId);
+                        if (rs != null) result = rs;
+                        else result = "failed";
+                    } else if (lId != null && boolDelete != null) {
+                        if(Boolean.parseBoolean(boolDelete)) {
+                            boolean rs = lDao.deleteLayout(lId);
+                            if (rs) result = "success";
+                            else result = "failed";
+                        }
+                    } else {
+                        int rs = lDao.getId();
+                        if (rs != -1) result = "" + rs;
+                        else result = "failed";
+                    }
+
+                    response.setContentType("text/plain");
+                    response.setCharacterEncoding("UTF-8");
+                    response.getWriter().write(result);
+                } else {
+                    JSONArray jsonObject = null;
+                    jsonObject = lDao.getAllLayouts(doc.getId());
+                    request.setCharacterEncoding("utf8");
+                    response.setContentType("text/plain");
+                    if (jsonObject != null) {
+                        PrintWriter out = response.getWriter();
+                        out.print(jsonObject.toString());
+                    } else {
+                        response.getWriter().write("Server Error");
+                    }
                 }
-            } else {
-                int rs = lDao.getId();
-                if (rs != -1) result = "" + rs;
-                else result = "failed";
             }
-
-            response.setContentType("text/plain");
-            response.setCharacterEncoding("UTF-8");
-            response.getWriter().write(result);
         } else {
-            JSONArray jsonObject = null;
-            jsonObject = lDao.getAllLayouts();
-            request.setCharacterEncoding("utf8");
-            response.setContentType("text/plain");
-            if (jsonObject != null) {
-                PrintWriter out = response.getWriter();
-                out.print(jsonObject.toString());
-            } else {
-                response.getWriter().write("Server Error");
-            }
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/view/login.jsp");
+            dispatcher.forward(request,response);
         }
+
     }
 }
